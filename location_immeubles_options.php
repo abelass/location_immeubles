40a32971b7d435667b581f04c3f08d0eb541843f
Rainer Müller <rainer@websimple.be>
2018-05-10 16:57:31 +0200

    la fabrique

---
+++ location_immeubles_options.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Options au chargement du plugin Location d&#039;immeubles
+ *
+ * @plugin     Location d&#039;immeubles
+ * @copyright  2018
+ * @author     Rainer Müller
+ * @licence    GNU/GPL v3
+ * @package    SPIP\Location_immeubles\Options
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/*
+ * Un fichier d'options permet de définir des éléments
+ * systématiquement chargés à chaque hit sur SPIP.
+ *
+ * Il vaut donc mieux limiter au maximum son usage
+ * tout comme son volume !
+ * 
+ */
---
+++ location_immeubles_pipelines.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Utilisations de pipelines par Location d&#039;immeubles
+ *
+ * @plugin     Location d&#039;immeubles
+ * @copyright  2018
+ * @author     Rainer Müller
+ * @licence    GNU/GPL v3
+ * @package    SPIP\Location_immeubles\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
