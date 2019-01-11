4a18b3fc8a2c070b0fad69c15b437a79ad62f2e0
abelass <rainer@websimple.be>
2019-01-11 16:28:01 +0100

    activement maintenu

--- location_immeubles_options.php
+++ location_immeubles_options.php
@@ -3,7 +3,7 @@
  * Options au chargement du plugin Location d&#039;immeubles
  *
  * @plugin     Location d&#039;immeubles
- * @copyright  2018
+ * @copyright  2018 - 2019
  * @author     Rainer Müller
  * @licence    GNU/GPL v3
  * @package    SPIP\Location_immeubles\Options
